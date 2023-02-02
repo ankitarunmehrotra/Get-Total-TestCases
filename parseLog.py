f58e53a85248e035e0cb40f7a7a6e5389996669a
Ankit.Mehrotra@sas.com <Ankit.Mehrotra@sas.com>
2023-02-02 21:29:31 +0530

    update to the function

--- parseLog.py
+++ parseLog.py
@@ -19,8 +19,6 @@ def getURLsFromString(urlRegex, message):
                     matching = [s for s in listOfURLs if url in s]
                     if(matching.__len__() == 0):
                         listOfURLs.append(url)
-                    
-        
     return listOfURLs;
 
 
@@ -46,7 +44,6 @@ def displayValuesOfInterest(jsonObject, extractionFields, stringObject):
    
    
 if __name__ == "__main__":
-    #baseDirectoryPath="C:\\Users\\sinanm\\Desktop\\logs\\"
     baseDirectoryPath="./sampleFiles/"
     #fileName = "sample-calls.txt"
     fileName = "sas-planning.txt"
@@ -60,12 +57,10 @@ if __name__ == "__main__":
     fileHandler.close()
     urlRegex = [ 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 
                  '/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+']
+
     for line in allLines:
-        #print(line + "\n")
         jsonLine = json.loads(line)
-        #print( jsonLine["level"] + "\t" + jsonLine["timeStamp"] + "\t" + jsonLine["message"] + "\t" + jsonLine["source"] + "\t" + jsonLine["properties"]["__session"] + "\t" + jsonLine["properties"]["username"])
         listOfURLs = getURLsFromString(urlRegex, jsonLine["message"])                      
-        #print(jsonLine["message"] + "\t : " + str(listOfURLs) )
     #    print( jsonLine["level"] + "\t" + jsonLine["timeStamp"] + "\t" + str(listOfURLs) + "\t" + jsonLine["source"] + "\t" + jsonLine["properties"]["__session"] + "\t" + jsonLine["properties"]["username"])
         
         printBuffer += displayValuesOfInterest(jsonLine, extractionFields, str(listOfURLs))
